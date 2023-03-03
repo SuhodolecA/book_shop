d65a672c326289b832a783c6f89fa135d391a82e
Anton Sukhadolets <suhodoleca@gmail.com>
2023-03-03 11:30:08 +0100

    feat: add basic footer section html markup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { wrapper } from "./modules/globalVars.js";
 import { createHeader } from "./modules/header.js";
+import { createFooter } from "./modules/footer.js";
 
 const fragment = document.createDocumentFragment();
 fragment.append(createHeader());
+fragment.append(createFooter());
 
 wrapper.append(fragment);
---
+++ js/modules/footer.js
@@ -0,0 +1,37 @@
+import { createElement } from "./helperFunctions.js";
+
+const createFooterContent = () => {
+  // create text information
+  const paragraph = createElement("p");
+  paragraph.classList.add("footer-text");
+  paragraph.textContent = "Coded by ";
+
+  //  create footer link
+  const link = createElement("a");
+  link.classList.add("footer-link");
+  link.href = "https://github.com/SuhodolecA";
+  link.setAttribute("target", "_blank");
+  link.textContent = "Anton Sukhadolets";
+
+  paragraph.append(link);
+  return paragraph;
+};
+
+const createFooter = () => {
+  // create footer
+  const footer = createElement("footer");
+  footer.classList.add("footer");
+
+  //  create container
+  const container = createElement("div");
+  container.classList.add("container");
+  // create footer content
+  const footerContent = createFooterContent();
+
+  container.append(footerContent);
+  footer.append(container);
+
+  return footer;
+};
+
+export { createFooter };
